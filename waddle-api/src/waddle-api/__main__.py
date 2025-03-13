1a09b7736d3917ee6135335d3431865b01f120ad
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-03-12 21:49:50 -0700

    feat: first endpoint

--- waddle-api/src/waddle-api/__main__.py
+++
@@ -1,4 +0,0 @@
-from template.aaa import bbb
-
-if __name__ == "__main__":
-    print(bbb())
--- waddle-api/tests/main_test.py
+++
@@ -1,6 +0,0 @@
-from waddle-api.aaa import bbb
-
-
-def test_pytest():
-    assert 1 == 1
-    assert bbb() == "bbb"
