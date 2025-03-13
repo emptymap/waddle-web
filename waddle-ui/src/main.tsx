45b92d79ce84356fa86c5e9a3e43ec79898e61aa
Shun Kashiwa <shunthedev@gmail.com>
2025-03-12 21:37:15 -0700

    setup router

--- waddle-ui/src/main.tsx
+++ waddle-ui/src/main.tsx
@@ -1,8 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
 
 const root = document.getElementById("root");
 
@@ -10,7 +21,7 @@ if (root) {
 	createRoot(root).render(
 		<StrictMode>
 			<Provider>
-				<App />
+				<RouterProvider router={router} />
 			</Provider>
 		</StrictMode>,
 	);
