e1e56a831bd4f0e44519c7c1ee248b7367d06d0c
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-03-14 18:25:06 -0700

    feat: update episode

--- waddle-api/app/models.py
+++ waddle-api/app/models.py
@@ -1,28 +1,17 @@
 from datetime import datetime
-from typing import Optional
 
 from nanoid import generate
 from sqlmodel import Field, SQLModel
 
 
 class Episode(SQLModel, table=True):
+    """Main episode model containing metadata and relationships to related resources"""
+
     uuid: str = Field(default_factory=generate, primary_key=True)
     preprocessed: bool = Field(default=False)
     postprocessed: bool = Field(default=False)
+    metadata_generated: bool = Field(default=False)
     editor_state: str = Field(default="")
     title: str = Field(default="")
     created_at: datetime = Field(default_factory=datetime.now)
-
-
-class EpisodeCreate(SQLModel):
-    title: str
-    editor_state: Optional[str] = ""
-    preprocessed: Optional[bool] = False
-    postprocessed: Optional[bool] = False
-
-
-class EpisodeUpdate(SQLModel):
-    title: Optional[str] = None
-    editor_state: Optional[str] = None
-    preprocessed: Optional[bool] = None
-    postprocessed: Optional[bool] = None
+    updated_at: datetime = Field(default_factory=datetime.now)
